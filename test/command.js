da1187f398d83d37b09dab750b1f87bc27f27e10
Doug Patti <douglas.patti@gmail.com>
2015-03-03 23:43:38 -0500

    some command stuff

---
+++ test/command.js
@@ -0,0 +1,7 @@
+var expect = require('chai').expect;
+
+describe('command', function() {
+  it('caches single files');
+  it('caches directories recursively');
+  it('throws if it does not succeed');
+});
